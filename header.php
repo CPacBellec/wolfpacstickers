a34f728cf4c1a4d8c1c9ae2260a522472985e8f3
wolfpac <cbellec@normandiewebschool.fr>
2023-12-27 00:29:52 +0100

    update + add styles.css + add background for my index

--- header.php
+++ header.php
@@ -8,20 +8,28 @@
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-  />
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+    <style>
+    body {
+        font-family: 'VT323', monospace;
+    }
+    </style>
 </head>
 <body class="flex flex-col min-h-screen">
     <header> 
     <nav class="bg-black p-4">
     <div class="container mx-auto flex justify-evenly items-center">
         <ul class="flex space-x-4">
-            <li><a href="index.php" class="text-white hover:text-gray-300">Accueil</a></li>
-            <li><a href="aboutme.php" class="text-white hover:text-gray-300">A propos</a></li>
+            <li><a href="index.php" class="text-slate-500 hover:text-gray-300">Accueil</a></li>
+            <li><a href="aboutme.php" class="text-slate-500 hover:text-gray-300">A propos</a></li>
         </ul>
-        <h1 class="text-white text-2xl font-bold">Wolfpac Stickers</h1>
+        <h1 class="text-slate-500 text-3xl font-bold">Wolfpac Stickers</h1>
         <ul class="flex space-x-4">
-            <li><a href="commissions.php" class="text-white hover:text-gray-300">Commissions</a></li>
-            <li><a href="contacts.php" class="text-white hover:text-gray-300">Contacts</a></li>
+            <li><a href="commissions.php" class="text-slate-500 hover:text-gray-300">Commissions</a></li>
+            <li><a href="contacts.php" class="text-slate-500 hover:text-gray-300">Contacts</a></li>
         </ul>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 include_once 'header.php';
 ?>
-    <section>
-        <img src="./assets/logo-wolfpac-sticker.png" alt="logo" class="">
+    <link rel="stylesheet" href="styles.css">
+
+    <section class="container flex flex-col">
+        <div class="flex justify-center items-center py-20">
+            <img src="./assets/logo-wolfpac-sticker.png" alt="logo" class="animate__animated animate__flipInX animate__slow">
+        </div>
         <h2>Bienvenue sur la page d'accueil</h2>
         <p>Contenu de la page d'accueil...</p>
     </section>
