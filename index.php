9bdce5ec4e6825ba52e9a8681ad6eda1d3948e13
wolfpac <cbellec@normandiewebschool.fr>
2023-12-28 17:38:52 +0100

    start responsive

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 include_once 'header.php';
 ?>
-    <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="styles.css">
 
-    <section class="container flex flex-col">
-        <div class="flex justify-center items-center">
-            <img src="./assets/logo-wolfpac-stickers-no-background.png" alt="logo" class="p-20 w-1/2 h-1/2 animate__animated animate__flipInX animate__slow">
-        </div>
-        <div class="flex justify-center items-center">
-            <h2 class="text-3xl">Bienvenue sur Wolfpac-Stickers, le site de stickers en pixel art</h2>
-            
-        </div>
-        
-    </section>
+<section class="container mx-auto my-8 flex flex-col items-center">
+    <div class="m-16 flex justify-center items-center">
+        <img src="./assets/logo-wolfpac-stickers-no-background.png" alt="logo" class="w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
+    </div>
+    <div class="text-center">
+        <h2 class="text-xl md:text-2xl lg:text-2xl xl:text-3xl">Bienvenue sur Wolfpac-Stickers, <br/> le site de stickers en pixel art</h2>
+    </div>
+</section>
 <?php
 include_once 'footer.php';
 ?>
+
