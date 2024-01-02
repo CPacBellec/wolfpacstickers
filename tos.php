b0f30a314749b3fe4979b4fab6bb957f1c2a1741
wolfpac <cbellec@normandiewebschool.fr>
2024-01-02 15:40:40 +0100

    update tos

--- tos.php
+++ tos.php
@@ -2,102 +2,120 @@
 include_once 'header.php';
 ?>
     <section class="max-w-2xl mx-auto mt-8">
-        <h2 class="text-2xl font-bold mb-4">Afin de prendre commande, veuillez lire et accepter mes CGU (Conditions Générales d'Utilisation)</h2>
+        <h2 class="text-3xl font-bold mb-4">Afin de prendre commande, veuillez lire et accepter mes CGU (Conditions Générales d'Utilisation)</h2>
         
 
-        A lire avant de prendre une commission - Les stickers sont seulement à usage personnel ( pour une utlilisation à visée commerciale , merci de me contacter en privé)
+        <p class="mb-4 text-xl">A lire avant de prendre une commission - Les stickers sont seulement à usage personnel ( pour une utlilisation à visée commerciale , merci de me contacter en privé)</p>
 
-        <div>
-            1 - Me contacter :
-
-            Sur mes réseaux sociaux (voir la page contact)
-
-            Vous pouvez me parler :
-
-            En Français
-            En Anglais
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">1 - Me contacter :</h3>
+            <p class="text-xl">
+                Sur mes réseaux sociaux (voir la page contact)<br/>
+                Vous pouvez me parler :<br/>
+                En Français <br/>
+                En Anglais
+            </p>
+            
         </div> 
-        <div>
-            2 - Bon sens:
-
-            Merci d'être poli.
-            Pas de spam
-            Je me donne le droit de refuser des commissions
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">2 - Bon sens:</h3>
+            <p class="text-xl">
+                Merci d'être poli.
+                Pas de spam
+                Je me donne le droit de refuser des commissions
+            </p>
+            
         </div>
-        <div>
-            3 - Paiement:
-
-            Je commence à travailler une fois le paiement effectué.
-            J'accepte que les paiements via PayPal et virements bancaires
-            Les prix sont non négociables.
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">3 - Paiement:</h3>
+            <p class="text-xl">
+                Je commence à travailler une fois le paiement effectué.
+                J'accepte que les paiements via PayPal et virements bancaires
+                Les prix sont non négociables.
+            </p>
+            
         </div>
-        <div>
-            4 - Qu'est-ce que je dessine?
-
-            Je fais les design :
-            En 16X16 ou en 32X32.
-            Du safe for work uniquement !
-            Sprite en couleurs
-            Sprite style gameboy
-            Objets
-            Coeurs
-
-            Je ne fais pas:
-            Du +18
-            Dessins érotiques/explicites
-            NSFW/ BDSM / Kinks
-            Gore
-            Sprites en taille supérieur à du 32X32.
-            Réalisme
-            Background
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">4 - Les commissions </h3>
+            <p class="text-xl">
+               Je fais les design :<br/>
+                En 16X16 ou en 32X32.<br/>
+                Du safe for work uniquement !<br/>
+                Sprite en couleurs<br/>
+                Sprite style gameboy<br/>
+                Objets<br/>
+                Coeurs<br/>
+                <br/>
+                Je ne fais pas:<br/>
+                Du +18<br/>
+                Dessins érotiques / explicites<br/>
+                NSFW/ BDSM / Kinks<br/>
+                Gore<br/>
+                Sprites en taille supérieur à du 32X32.<br/>
+                Réalisme<br/>
+                Background<br/> 
+            </p>
+            
         </div>
-        <div>
-            5 - Passage de commande:
-
-            Contactez-moi afin pouvoir prendre une commission (dans la limite des places disponibles)
-            Payement sur Paypal ou virement bancaire.
-            Envoyez moi une ou des références, sinon décrivez moi au mieux ce que vous désirez et donnez le plus de détails possible afin que de correspondre à votre demande.
-            Validation du premier design. Vous pouvez signaler autant de changement que vous le souhaitez sans frais additionnels. Je serai ravi de correspondre au plus près à vôtre requête.
-            Je vous envoie une première version du design complété .
-            A noter que vous pouvez commander plusieurs stickers de votre design (attention, cela engendra des frais supplémentaires).
-            Des frais pourront s'appliquer, en cas de demandes de changement majeures.
-            Une fois le/les sticker/s fini/s, je vous envoie la commande.     
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">5 - Passage de commande:</h3>
+            <p class="text-xl">
+               Contactez-moi afin pouvoir prendre une commission (dans la limite des places disponibles).
+                Payement sur Paypal ou virement bancaire.
+                Envoyez moi une ou des références, sinon décrivez moi au mieux ce que vous désirez et donnez le plus de détails possible afin que de correspondre à votre demande.
+                Validation du premier design. Vous pouvez signaler autant de changement que vous le souhaitez sans frais additionnels. Je serai ravi de correspondre au plus près à vôtre requête.
+                Je vous envoie une première version du design complété .
+                A noter que vous pouvez commander plusieurs stickers de votre design (attention, cela engendra des frais supplémentaires).
+                Des frais pourront s'appliquer, en cas de demandes de changement majeures.
+                Une fois le/les sticker/s fini/s, je vous envoie la commande. 
+            </p>
+                 
         </div>
-        <div>
-            6 - La Liste d'attente:
-
-            J'y place sur cette liste toutes les personnes potentiellement intéressées, la commission étant officiellement prise en compte lorsque le payement est terminé.
-            Etre en liste d'attente ne vous pousse pas à vous y engager. Merci de me prévenir en avance si vous n'êtes plus intéressé, si vous ne pouvez pas payer ou si vous repoussez vôtre tour.
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">6 - La Liste d'attente:</h3>
+            <p class="text-xl">
+                J'y place sur cette liste toutes les personnes potentiellement intéressées, la commission étant officiellement prise en compte lorsque le payement est terminé.
+                Etre en liste d'attente ne vous pousse pas à vous y engager. Merci de me prévenir en avance si vous n'êtes plus intéressé, si vous ne pouvez pas payer ou si vous repoussez vôtre tour.
+            </p>
+            
         </div>
-        <div>
-            7 - Annulations:
-
-            Il n'y a pas de remboursements en général.
-            Je ne rembourse quelqu'un seulement si je ne suis pas en mesure de finir le design ou dans des cas particuliers (examens, problèmes matériels).
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">7 - Annulations:</h3>
+            <p class="text-xl">
+                Il n'y a pas de remboursements en général.
+                Je ne rembourse quelqu'un seulement si je ne suis pas en mesure de finir le design ou dans des cas particuliers (examens, problèmes matériels).
+            </p>
+            
 
         </div>
-        <div>
-            8 - Livraison:
-
-            Je vous tiendrais au courant de mon avancement autant que possible!
-            La durée d'attente pour un projet démarrera en fonction de votre place dans ma waiting list, post-paiement.
-            Je n'ai pas de deadline défini : je fais en fonction de mes disponibilités.
-            Pour des deadlines particulières (anniversaire, noël), merci de me le mentionner, savoir si c'est réalisable et si cela peut couter un supplément.
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">8 - Livraison:</h3>
+            <p class="text-xl">
+                Je vous tiendrais au courant de mon avancement autant que possible!
+                La durée d'attente pour un projet démarrera en fonction de votre place dans ma liste d'attente, post-paiement.
+                Aucune deadline : je fais en fonction de mes disponibilités.
+                Pour des deadlines particulières (anniversaire, noël), merci de me le mentionner, savoir si c'est réalisable et si cela peut couter un supplément.
+            </p>
+            
         </div>
-        <div>
-            9 - A ne pas faire :
-
-            Faire des modifications sur mon dessin sans autorisation au préalable.
-            Faire de la revente de mon design.
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">9 - A ne pas faire :</h3>
+            <p class="text-xl">
+                Faire des modifications sur mon dessin sans autorisation au préalable.
+                Faire de la revente de mon design.  
+            </p>
+            
         </div>
-        <div>
-            10 -  Extras:
-
-            N'oubliez pas de me crédité une fois votre commande recu.
-            Je reposterai les designs sur mes réseaux sociaux.
-            Si le sticker est sensé être une surprise, merci de me l'indiquer.
-            N'hésitez pas à me donner vos avis
-            Contactez-moi, si jamais vous avez des questions!
+        <div class="mb-8">
+            <h3 class="text-2xl font-bold mb-4">10 -  Extras :</h3>
+            <p class="text-xl">
+                N'oubliez pas de me créditer une fois votre commande recu.
+                Je reposterai les designs sur mes réseaux sociaux.
+                Si le sticker est sensé être une surprise, merci de me l'indiquer.
+                N'hésitez pas à me donner vos avis.
+                Si vous avez des questions, n'hésitez pas à me contacter !
+            </p>
+            
         </div>
     </section>
 <?php
