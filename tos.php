2f3fe0324829b754e71d5bc273a6c8a938decaa0
wolfpac <cbellec@normandiewebschool.fr>
2024-01-04 16:10:50 +0100

    update

--- tos.php
+++ tos.php
@@ -6,110 +6,108 @@ include_once 'header.php';
             <div class=" flex justify-center items-center">
                 <img src="./assets/cgu-sprite.png" alt="logo" class="flex justify-center items-center w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
             </div>
-            
-        
             <h2 class="text-3xl font-bold mb-4">Afin de prendre commande, veuillez lire et accepter mes CGU (Conditions Générales d'Utilisation)</h2>
             <p class="mb-4 text-xl">A lire avant de prendre une commission - Les stickers sont seulement à usage personnel ( pour une utlilisation à visée commerciale , merci de me contacter en privé)</p>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">1 - Me contacter :</h3>
-            <p class="text-xl">
-                Sur mes réseaux sociaux (voir la page contact)<br/>
-                Vous pouvez me parler :<br/>
-                En Français <br/>
-                En Anglais
-            </p>            
-        </div> 
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">2 - Bon sens:</h3>
-            <p class="text-xl">
-                Merci d'être poli.<br/>
-                Pas de spam<br/>
-                Je me donne le droit de refuser des commissions.
-            </p>            
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">3 - Paiement:</h3>
-            <p class="text-xl">
-                Je commence à travailler une fois le paiement effectué.<br/>
-                J'accepte que les paiements via PayPal et virements bancaires.<br/>
-                Les prix ne sont pas négociables.
-            </p>          
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">4 - Les commissions </h3>
-            <p class="text-xl">
-               Je fais les design :<br/>
-                En 16X16 ou en 32X32.<br/>
-                Du safe for work uniquement !<br/>
-                Sprite en couleurs<br/>
-                Sprite style gameboy<br/>
-                Objets<br/>
-                Coeurs<br/>
-                <br/>
-                Je ne fais pas:<br/>
-                Du +18<br/>
-                Dessins érotiques / explicites<br/>
-                NSFW/ BDSM / Kinks<br/>
-                Gore<br/>
-                Sprites en taille supérieur à du 32X32.<br/>
-                Réalisme<br/>
-                Background<br/> 
-            </p>       
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">5 - Passage de commande:</h3>
-            <p class="text-xl">
-               Contactez-moi afin pouvoir prendre une commission (dans la limite des places disponibles).<br/>
-                Payement sur Paypal ou virement bancaire.<br/>
-                Envoyez moi une ou des références, sinon décrivez moi au mieux ce que vous désirez et donnez le plus de détails possible afin que de correspondre à votre demande.<br/>
-                Validation du premier design. Vous pouvez signaler autant de changement que vous le souhaitez sans frais additionnels. Je serai ravi de correspondre au plus près à vôtre requête.<br/>
-                Je vous envoie une première version du design complété .<br/>
-                A noter que vous pouvez commander plusieurs stickers de votre design (attention, cela engendra des frais supplémentaires).<br/>
-                Des frais pourront s'appliquer, en cas de demandes de changement majeures.<br/>
-                Une fois le/les sticker/s fini/s, je vous envoie la commande. 
-            </p>                
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">6 - La Liste d'attente:</h3>
-            <p class="text-xl">
-                J'y place sur cette liste toutes les personnes potentiellement intéressées, la commission étant officiellement prise en compte lorsque le payement est terminé.
-                Etre en liste d'attente ne vous pousse pas à vous y engager. Merci de me prévenir en avance si vous n'êtes plus intéressé, si vous ne pouvez pas payer ou si vous repoussez vôtre tour.
-            </p>
-            
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">7 - Annulations:</h3>
-            <p class="text-xl">
-                Il n'y a pas de remboursements en général.
-                Je ne rembourse quelqu'un seulement si je ne suis pas en mesure de finir le design ou dans des cas particuliers (examens, problèmes matériels).
-            </p>
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">8 - Livraison:</h3>
-            <p class="text-xl">
-                Je vous tiendrais au courant de mon avancement autant que possible!<br/>
-                La durée d'attente pour un projet démarrera en fonction de votre place dans ma liste d'attente, post-paiement.<br/>
-                Aucune deadline : je fais en fonction de mes disponibilités.<br/>
-                Pour des deadlines particulières (anniversaire, noël), merci de me le mentionner, savoir si c'est réalisable et si cela peut couter un supplément.
-            </p>            
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">9 - A ne pas faire :</h3>
-            <p class="text-xl">
-                Faire des modifications sur mon dessin sans autorisation au préalable.<br/>
-                Faire de la revente de mon design.  
-            </p>           
-        </div>
-        <div class="mb-8">
-            <h3 class="text-2xl font-bold mb-4">10 -  Extras :</h3>
-            <p class="text-xl">
-                N'oubliez pas de me créditer une fois votre commande recu.<br/>
-                Je reposterai les designs sur mes réseaux sociaux.<br/>
-                Si le sticker est sensé être une surprise, merci de me l'indiquer.<br/>
-                N'hésitez pas à me donner vos avis.<br/>
-                Si vous avez des questions, n'hésitez pas à me contacter !
-            </p>           
-        </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">1 - Me contacter :</h3>
+                <p class="text-xl">
+                    Sur mes réseaux sociaux (voir la page contact)<br/>
+                    Vous pouvez me parler :<br/>
+                    En Français <br/>
+                    En Anglais
+                </p>            
+            </div> 
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">2 - Bon sens:</h3>
+                <p class="text-xl">
+                    Merci d'être poli.<br/>
+                    Pas de spam<br/>
+                    Je me donne le droit de refuser des commissions.
+                </p>            
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">3 - Paiement:</h3>
+                <p class="text-xl">
+                    Je commence à travailler une fois le paiement effectué.<br/>
+                    J'accepte que les paiements via PayPal et virements bancaires.<br/>
+                    Les prix ne sont pas négociables.
+                </p>          
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">4 - Les commissions </h3>
+                <p class="text-xl">
+                Je fais les design :<br/>
+                    En 16X16 ou en 32X32.<br/>
+                    Du safe for work uniquement !<br/>
+                    Sprite en couleurs<br/>
+                    Sprite style gameboy<br/>
+                    Objets<br/>
+                    Coeurs<br/>
+                    <br/>
+                    Je ne fais pas:<br/>
+                    Du +18<br/>
+                    Dessins érotiques / explicites<br/>
+                    NSFW/ BDSM / Kinks<br/>
+                    Gore<br/>
+                    Sprites en taille supérieur à du 32X32.<br/>
+                    Réalisme<br/>
+                    Background<br/> 
+                </p>       
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">5 - Passage de commande:</h3>
+                <p class="text-xl">
+                Contactez-moi afin pouvoir prendre une commission (dans la limite des places disponibles).<br/>
+                    Payement sur Paypal ou virement bancaire.<br/>
+                    Envoyez moi une ou des références, sinon décrivez moi au mieux ce que vous désirez et donnez le plus de détails possible afin que de correspondre à votre demande.<br/>
+                    Validation du premier design. Vous pouvez signaler autant de changement que vous le souhaitez sans frais additionnels. Je serai ravi de correspondre au plus près à vôtre requête.<br/>
+                    Je vous envoie une première version du design complété .<br/>
+                    A noter que vous pouvez commander plusieurs stickers de votre design (attention, cela engendra des frais supplémentaires).<br/>
+                    Des frais pourront s'appliquer, en cas de demandes de changement majeures.<br/>
+                    Une fois le/les sticker/s fini/s, je vous envoie la commande. 
+                </p>                
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">6 - La Liste d'attente:</h3>
+                <p class="text-xl">
+                    J'y place sur cette liste toutes les personnes potentiellement intéressées, la commission étant officiellement prise en compte lorsque le payement est terminé.
+                    Etre en liste d'attente ne vous pousse pas à vous y engager. Merci de me prévenir en avance si vous n'êtes plus intéressé, si vous ne pouvez pas payer ou si vous repoussez vôtre tour.
+                </p>
+                
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">7 - Annulations:</h3>
+                <p class="text-xl">
+                    Il n'y a pas de remboursements en général.
+                    Je ne rembourse quelqu'un seulement si je ne suis pas en mesure de finir le design ou dans des cas particuliers (examens, problèmes matériels).
+                </p>
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">8 - Livraison:</h3>
+                <p class="text-xl">
+                    Je vous tiendrais au courant de mon avancement autant que possible!<br/>
+                    La durée d'attente pour un projet démarrera en fonction de votre place dans ma liste d'attente, post-paiement.<br/>
+                    Aucune deadline : je fais en fonction de mes disponibilités.<br/>
+                    Pour des deadlines particulières (anniversaire, noël), merci de me le mentionner, savoir si c'est réalisable et si cela peut couter un supplément.
+                </p>            
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">9 - A ne pas faire :</h3>
+                <p class="text-xl">
+                    Faire des modifications sur mon dessin sans autorisation au préalable.<br/>
+                    Faire de la revente de mon design.  
+                </p>           
+            </div>
+            <div class="mb-8">
+                <h3 class="text-2xl font-bold mb-4">10 -  Extras :</h3>
+                <p class="text-xl">
+                    N'oubliez pas de me créditer une fois votre commande recu.<br/>
+                    Je reposterai les designs sur mes réseaux sociaux.<br/>
+                    Si le sticker est sensé être une surprise, merci de me l'indiquer.<br/>
+                    N'hésitez pas à me donner vos avis.<br/>
+                    Si vous avez des questions, n'hésitez pas à me contacter !
+                </p>           
+            </div>
         </div>
     </section>
 <?php
