614546b72756dde523283e8bd3aa67ea483e3b86
wolfpac <cbellec@normandiewebschool.fr>
2024-01-05 16:08:44 +0100

    add new asset + new img on commissions page

--- commissions.php
+++ commissions.php
@@ -2,6 +2,9 @@
 include_once 'header.php';
 ?>
     <section>
+        <div class=" flex justify-center items-center">
+            <img src="./assets/commission-sprite.png" alt="logo" class="flex justify-center items-center w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
+        </div>
         <h2>Page des commissions</h2>
         <p>Contenu de la page des commissions...</p>
     </section>
