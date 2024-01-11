5963559a2f2bc5d0b55732f4a2871c6b82bf2189
wolfpac <cbellec@normandiewebschool.fr>
2024-01-11 11:20:19 +0100

    update portfolio

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once 'header.php';
         <img src="./assets/logo-wolfpac-stickers-no-background.png" alt="logo" class="w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
     </div>
     <div class="text-center">
-        <h2 class="text-xl md:text-2xl lg:text-2xl xl:text-3xl">Bienvenue sur Wolfpac-Stickers, <br/> le site de stickers en pixel art</h2>
+        <h2 class="text-xl md:text-2xl lg:text-2xl xl:text-3xl">Bienvenue sur Wolfpac Stickers, <br/> le site de stickers en pixel art</h2>
     </div>
 </section>
 <?php
