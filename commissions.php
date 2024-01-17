b99f9f6bda86c4c01d344e652572db0dba0c169c
wolfpac <cbellec@normandiewebschool.fr>
2024-01-17 11:04:00 +0100

    update responsive

--- commissions.php
+++ commissions.php
@@ -1,9 +1,9 @@
 <?php
 include_once 'header.php';
 ?>
-    <div class="bg-indigo-300 min-h-screen">
+    <div class="bg-indigo-300 min-h-screen p-8">
         <div class=" flex justify-center items-center">
-            <img src="./assets/commission-sprite.png" alt="logo" class="flex justify-center items-center w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
+            <img src="./assets/commission-sprite.png" alt="logo" class="flex justify-center items-center md:w-1/2 lg:w-9/12 h-auto animate__animated animate__flipInX animate__slow">
         </div>
         <section id="commissions" class="max-w-2xl mx-auto py-5">
             <div>
@@ -16,17 +16,20 @@ include_once 'header.php';
                     [
                         'title' => 'Coeur',
                         'image' => 'assets/full-red-heart-export.png',
-                        'description' => 'De petits coeurs en format 16X16 pixels. La forme et les couleurs sont modifiables à la demande. Prix : WIP',
+                        'description' => 'De petits coeurs en format 16X16 pixels. La forme et les couleurs sont modifiables à la demande.',
+                        'price' => 'Prix : WIP',
                     ],
                     [
                         'title' => 'Style Gameboy',
                         'image' => 'assets/wolf-gameboy-style-export.png',
-                        'description' => "Des personnages dans le style de la première GameBoy en format 16X16. L'apparence est modifiable à la demande. Prix : WIP ",
-                    ],
+                        'description' => "Des personnages dans le style de la première GameBoy en format 16X16. L'apparence est modifiable à la demande.",
+                        'price' => 'Prix : WIP',
+                    ],  
                     [
                         'title' => 'Personnage couleur',
                         'image' => 'assets/bear-test-export.png',
-                        'description' => "Des personnages colorisés en format 16X16. L'apparence et les couleurs sont modifiables à la demande. Prix : WIP ",
+                        'description' => "Des personnages colorisés en format 16X16. L'apparence et les couleurs sont modifiables à la demande.",
+                        'price' => 'Prix : WIP',
                     ],
                     // Ajoutez autant d'éléments que nécessaire
                 ];
@@ -34,11 +37,12 @@ include_once 'header.php';
                 foreach ($cards as $card) {
                     ?>
                     <div class="flex">
-                        <div class="flex max-w-md mx-auto bg-white rounded overflow-hidden shadow-lg my-4">
+                        <div class="md:flex lg:flex-row md:max-w-lg lg:max-w-xl mx-auto bg-white rounded overflow-hidden shadow-lg my-4">
                             <img class="w-full" src="<?php echo $card['image']; ?>" alt="<?php echo $card['title']; ?>">
                             <div class="px-6 py-4">
                                 <div class="font-bold text-xl mb-2"><?php echo $card['title']; ?></div>
                                 <p class="text-gray-700 text-base"><?php echo $card['description']; ?></p>
+                                <p class="text-gray-700 text-base"><?php echo $card['price']; ?></p>
                             </div>
                         </div>
                     </div>
@@ -51,7 +55,7 @@ include_once 'header.php';
 
         <section id="portfolio" class=" mx-auto py-5">
             <div class=" flex justify-center items-center">
-                <img src="./assets/portfolio-sprite.png" alt="logo" class="flex justify-center items-center w-9/12 md:w-1/2 lg:w-7/12 xl:w-12/20 h-auto animate__animated animate__flipInX animate__slow">
+                <img src="./assets/portfolio-sprite.png" alt="logo" class="flex justify-center items-center md:w-1/2 lg:w-9/12 h-auto animate__animated animate__flipInX animate__slow">
             </div>
             <div>
                 <h2 class="text-3xl font-bold mb-4 text-center">Zone portfolio</h2>
@@ -67,7 +71,7 @@ include_once 'header.php';
                 $portfolioImages = glob($portfolioFolder . '*.{jpg,png,gif}', GLOB_BRACE);
 
                 // Divisez les images en groupes de 6
-                $imageGroups = array_chunk($portfolioImages, 6);
+                $imageGroups = array_chunk($portfolioImages, 3);
 
                 // Affichez les images par groupe
                 foreach ($imageGroups as $group) {
@@ -76,7 +80,7 @@ include_once 'header.php';
                         <?php
                         foreach ($group as $image) {
                             ?>
-                            <div class="max-w-sm mx-auto  overflow-hidden  my-4 px-2">
+                            <div class="max-w-sm mx-auto overflow-hidden  my-4 px-2">
                                 <img class="w-full" src="<?php echo $image; ?>" alt="Portfolio Image">
                             </div>
                             <?php
